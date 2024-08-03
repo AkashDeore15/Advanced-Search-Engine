bb4f196b77189d4c4a24c13a4899ce61eaecc4f4
Akash Deore <69108362+AkashDeore15@users.noreply.github.com>
2024-08-04 02:11:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-# Simple Search Engine
+# Advanced Search Engine
 
-This project is a simple search engine built using Python. It indexes the content of web pages and allows you to search for keywords across those pages.
+This project is an advanced search engine built using Python. It indexes the content of web pages and allows you to search for keywords across those pages using TF-IDF for finding important words and Redis for fast data retrieval.
 
 ## Features
 
 - Fetches content from specified URLs
-- Indexes the content of the pages
+- Indexes the content of the pages using Redis
+- Uses TF-IDF to find important words and rank search results
 - Searches for keywords in the indexed content
 
 ## Installation
 
 1. Clone the repository:
    ```bash
-   git clone https://github.com/AkashDeore15/search_engine_project.git
-   cd search_engine_project
-
-"# Search_Engine_Project" 
+   git clone https://github.com/AkashDeore15/advanced_search_engine.git
+   cd advanced_search_engine
--- search_engine.py
+++ search_engine.py
@@ -1,43 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+import redis
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-class SimpleSearchEngine:
+class AdvancedSearchEngine:
     def __init__(self, urls):
         self.urls = urls
-        self.index = {}
+        self.index = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.documents = []
+        self.vectorizer = TfidfVectorizer()
         self.create_index()
 
     def create_index(self):
         for url in self.urls:
             content = self.fetch_content(url)
+            self.documents.append(content)
             self.index_content(url, content)
+        self.create_tfidf_index()
 
     def fetch_content(self, url):
         response = requests.get(url)
-        return response.text
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup.get_text()
 
     def index_content(self, url, content):
-        soup = BeautifulSoup(content, 'html.parser')
-        text = soup.get_text().lower()
-        words = text.split()
-        for word in words:
-            if word in self.index:
-                self.index[word].add(url)
-            else:
-                self.index[word] = {url}
+        self.index.set(url, content)
+
+    def create_tfidf_index(self):
+        tfidf_matrix = self.vectorizer.fit_transform(self.documents)
+        self.tfidf_matrix = tfidf_matrix.toarray()
+        self.features = self.vectorizer.get_feature_names_out()
 
     def search(self, query):
-        query = query.lower()
-        results = self.index.get(query, set())
+        query_vec = self.vectorizer.transform([query]).toarray()[0]
+        similarities = np.dot(self.tfidf_matrix, query_vec)
+        sorted_similarities = np.argsort(similarities)[::-1]
+        results = [(self.urls[i], similarities[i]) for i in sorted_similarities if similarities[i] > 0]
         return results
 
 if __name__ == "__main__":
     urls = [
-        "https://www.google.com",
-        "https://mail.google.com",
+        "https://www.msn.com/en-in/sports/olympics?ocid=msedgntp",
+        "https://www.msn.com/en-in/feed?ocid=msedgntp",
         "https://www.njit.edu/bursar/important-dates"
     ]
-    search_engine = SimpleSearchEngine(urls)
+    search_engine = AdvancedSearchEngine(urls)
     query = input("Enter a search query: ")
     results = search_engine.search(query)
     if results:
